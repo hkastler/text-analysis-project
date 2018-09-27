1668641138f4da4a56c46993eda9c97f97477354
hkastler <hkastler+github@gmail.com>
2018-09-27 08:13:10 -0500

    microservice volume fix
    need to study loading resource files from jars within wars

--- text-analysis-service/buildAndRun.sh
+++ text-analysis-service/buildAndRun.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mvn clean package && docker build -t com.hkstlr/text-analysis-microservice .
-docker rm -f text-analysis-microservice || true && docker run -p 8080:8080 -p 4848:4848 --name text-analysis-microservice com.hkstlr/text-analysis-microservice 
+mvn clean package && docker build -t com.hkstlr/text-analysis-service .
+docker rm -f text-analysis-service || true && docker run -v c:/etc/config:/etc/config -p 8080:8080 -p 4848:4848 --name text-analysis-service com.hkstlr/text-analysis-service 
