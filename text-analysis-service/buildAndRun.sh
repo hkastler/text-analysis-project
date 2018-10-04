07f778cf19aab1918ad73940a17ee263107184b1
hkastler <hkastler+github@gmail.com>
2018-10-04 11:12:11 -0500

    volume mount path

--- text-analysis-service/buildAndRun.sh
+++ text-analysis-service/buildAndRun.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mvn clean package && docker build -t com.hkstlr/text-analysis-service .
-docker rm -f text-analysis-service || true && docker run -v c:/etc/config:/etc/config -p 8080:8080 -p 4848:4848 --name text-analysis-service com.hkstlr/text-analysis-service 
+docker rm -f text-analysis-service || true && docker run -v c:/etc/opt/text-analysis-project:/etc/opt/text-analysis-project -p 8080:8080 -p 4848:4848 --name text-analysis-service com.hkstlr/text-analysis-service 
