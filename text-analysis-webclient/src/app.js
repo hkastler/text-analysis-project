ae8c52aeb039dc1f92ed29b073574db9f0906271
hkastler <hkastler+github@gmail.com>
2018-10-10 19:33:22 -0500

    service change, form enhancement, navbar active

--- text-analysis-webclient/src/app.js
+++ text-analysis-webclient/src/app.js
@@ -32,10 +32,16 @@
                 });
 
         })
+        .controller('navCtrl', function ($scope, $location) {
+            $scope.isActive = function (viewLocation) {
+                return viewLocation === $location.path();
+            };
+        })
         .controller('mainCtrl', function ($log, $scope, $http) {
 
 
-            $scope.queryTerms = 'pizza';
+            $scope.queryTerms = 'chicago pizza';
+            $scope.tweetCount = 100;
 
             $scope.resultsTotal = '';
             $scope.resultsData = [];
@@ -46,45 +52,47 @@
 
             $scope.getResults = function () {
 
-                $http.get(serviceLoc + urlBase + 'results/' + $scope.queryTerms)
+                $http.get(serviceLoc + urlBase + 'sa/' + $scope.queryTerms + "/" + $scope.tweetCount)
                     .then(function (data) {
 
                         $scope.resultsTotal = data.data[0];
-                        var psv = d3.dsvFormat(";");
-                        $scope.resultsData = psv.parseRows(data.data[1]);
-
-                        var container = d3.select("#resultsTable");
-                        container.html('');
-
-                        var table = d3.select("#resultsTable").append("table")
-                            .attr("class", "table table-striped");
-
-                        //proper table header 
-                        var thead = table.append("thead");
-                        thead.selectAll("th")
-                        .data($scope.resultsData[0]).enter()
-                        .append("th")
-                        .text(function (column) { return column; });                        
-                        //this deletes the header row
-                        $scope.resultsData.shift();
-
-                        //proper table body
-                        var tbody = table.append("tbody");
-
-                        tbody.selectAll("tr")
-                        //output the remaining data
-                            .data($scope.resultsData).enter()
-                            .append("tr")
-
-                            .selectAll("td")
-                            .data(function (d) { return d; }).enter()
-                            .append("td")
-                            .text(function (d) { return d; });
-
+                        tabulateDsv(data.data[1],";", "#resultsTable");
 
                     });
 
             };
+            function tabulateDsv(dsvdata, delimiter, target) {
+                
+                var rows = d3.dsvFormat(delimiter).parseRows(dsvdata);
+
+                var container = d3.select(target);
+                container.html('');
+
+                var table = d3.select(target).append("table")
+                    .attr("class", "table table-striped");
+
+                //proper table header 
+                var thead = table.append("thead").append("tr");
+                thead.selectAll("th")
+                    .data(rows[0]).enter()
+                    .append("th")
+                    .text(function (column) { return column; });
+                //this deletes the header row
+                rows.shift();
+
+                //proper table body
+                var tbody = table.append("tbody");
+
+                tbody.selectAll("tr")
+                    //output the remaining data
+                    .data(rows).enter()
+                    .append("tr")
+                    .selectAll("td")
+                    .data(function (d) { return d; }).enter()
+                    .append("td")
+                    .text(function (d) { return d; });
+               
+            }
 
         });
 })();
