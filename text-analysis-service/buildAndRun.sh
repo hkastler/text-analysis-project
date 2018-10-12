607419bc1eeffd8bf7f0f219e2a25d41322da0d4
hkastler <hkastler+github@gmail.com>
2018-10-12 07:51:08 -0500

    Docker environment changes
    enviroment change from glassfish to wildfly. develop the angular app on the local filesystem, mount volume, serve through undertow in wildfly (now at v14).

--- text-analysis-service/buildAndRun.sh
+++ text-analysis-service/buildAndRun.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 mvn clean package && docker build -t com.hkstlr/text-analysis-service .
-docker rm -f text-analysis-service || true && docker run -v c:/etc/opt/text-analysis-project:/etc/opt/text-analysis-project -p 8080:8080 -p 4848:4848 --name text-analysis-service com.hkstlr/text-analysis-service 
+OPT_PATH=c:/etc/opt/text-analysis-project
+WEBCLIENT_PATH=c:/Users/henry.kastler/Documents/GitHub/text-analysis-project/text-analysis-webclient
+docker rm -f text-analysis-service || true && docker run -v $OPT_PATH:/etc/opt/text-analysis-project -v $WEBCLIENT_PATH:/app/default -p 8080:8080 -p 4848:4848 --name text-analysis-service com.hkstlr/text-analysis-service 
