0cce72a1677ea559560763a8518dfeb2ff4dd04d
hkastler <hkastler+github@gmail.com>
2018-10-12 10:56:52 -0500

    clean install all needed projects

--- text-analysis-service/buildAndRun.sh
+++ text-analysis-service/buildAndRun.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-mvn clean package && docker build -t com.hkstlr/text-analysis-service .
+mvn clean install -f ../text-analysis/pom.xml
+mvn clean install -f ../text-analysis-twitter/pom.xml
+mvn clean install && docker build -t com.hkstlr/text-analysis-service .
 OPT_PATH=c:/etc/opt/text-analysis-project
 WEBCLIENT_PATH=c:/Users/henry.kastler/Documents/GitHub/text-analysis-project/text-analysis-webclient
 docker rm -f text-analysis-service || true && docker run -v $OPT_PATH:/etc/opt/text-analysis-project -v $WEBCLIENT_PATH:/app/default -p 8080:8080 -p 4848:4848 --name text-analysis-service com.hkstlr/text-analysis-service 
