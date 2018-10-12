c91a4beceba583e04edcd00c45a6f80425d28ded
hkastler <hkastler+github@gmail.com>
2018-10-12 07:52:44 -0500

    change to object from string

--- text-analysis-service/src/main/java/com/hkstlr/rest/twitter/boundary/TwitterSAService.java
+++ text-analysis-service/src/main/java/com/hkstlr/rest/twitter/boundary/TwitterSAService.java
@@ -1,17 +1,22 @@
 package com.hkstlr.rest.twitter.boundary;
 
+import java.io.IOException;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.json.JsonBuilderFactory;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.codehaus.jackson.map.ObjectMapper;
 
 import com.hkstlr.twitter.control.TweetAnalyzer;
 
@@ -41,27 +46,28 @@ public class TwitterSAService {
 	@GET
 	@Path("/results/{queryTerms}")
 	@Produces(MediaType.APPLICATION_JSON)
-    public String[] getResults(@DefaultValue(value = "pizza") @PathParam("queryTerms") String queryTerms) {
+    public Object[] getResults(@DefaultValue(value = "pizza") @PathParam("queryTerms") String queryTerms) {
 		return getSentimentAnalysis(queryTerms, ta.getTweetCount());
     }
 	@GET
 	@Path("/sa/{queryTerms}/{tweetCount}")
 	@Produces(MediaType.APPLICATION_JSON)
-    public String[] getSA(@PathParam("queryTerms") String queryTerms,
+    public Object[] getSA(@PathParam("queryTerms") String queryTerms,
     		@PathParam("tweetCount") int tweetCount) {
 		return getSentimentAnalysis(queryTerms, tweetCount);
     }
 	
-	private String[] getSentimentAnalysis(String queryTerms, int tweetCount) {
+	private Object[] getSentimentAnalysis(String queryTerms, int tweetCount) {
 		Object[] results = null;
 		try {
 			results = (Object[]) ta.getSentimentAnalysis(queryTerms,tweetCount);
 		} catch (TwitterException e) {
 			LOG.log(Level.INFO,"", e);
 		}
+		Map<String,String> resultMap = (Map<String, String>) results[0];
 		String csv = (String)results[1];
-		String[] obj = new String[2];
-		obj[0] = results[0].toString();
+		Object[] obj = new Object[2];
+		obj[0] = resultMap;
 		obj[1] = csv;
 
 		return obj;
