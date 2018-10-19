5b54e63c1acffe028c497d0d15079f587c19dcd9
hkastler <hkastler+github@gmail.com>
2018-10-19 08:10:10 -0500

    wildfly admin and maven plugin

--- text-analysis-service/buildAndRun.bat
+++ text-analysis-service/buildAndRun.bat
@@ -6,4 +6,4 @@ call mvn clean install
 call docker build -t com.hkstlr/text-analysis-service .
 SET OPT_PATH=c:/etc/opt/text-analysis-project
 SET WEBCLIENT_PATH=c:/Users/henry.kastler/Documents/GitHub/text-analysis-project/text-analysis-webclient
-call docker rm -f text-analysis-service & docker run -v %OPT_PATH%:/etc/opt/text-analysis-project -v %WEBCLIENT_PATH%:/app/default -p 8080:8080 -p 8443:8443 --name text-analysis-service com.hkstlr/text-analysis-service 
+call docker rm -f text-analysis-service & docker run -v %OPT_PATH%:/etc/opt/text-analysis-project -v %WEBCLIENT_PATH%:/app/default -p 8080:8080 -p 8443:8443 -p 9990:9990 --name text-analysis-service com.hkstlr/text-analysis-service 
