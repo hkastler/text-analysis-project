45c9d323f02aea7e88769b8e1d9593f999cbae9c
hkastler <hkastler+github@gmail.com>
2018-10-24 15:52:21 -0500

    test

---
+++ text-analysis-service/src/test/java/com/hkstlr/rest/JAXRSConfigurationTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hkstlr.rest;
+
+import javax.servlet.ServletException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author henry.kastler
+ */
+public class JAXRSConfigurationTest {
+    
+    JAXRSConfiguration cut;
+    public JAXRSConfigurationTest() {
+    }
+    
+    
+
+    @Test
+    public void testJAXRSConfiguration() {
+        cut = new JAXRSConfiguration();
+        Assertions.assertNotNull(cut);
+    }
+    
+}
