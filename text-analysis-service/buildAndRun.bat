6c4b39baced2819d159365b24cf17d4dc4bcf1bc
hkastler <hkastler+github@gmail.com>
2018-11-14 10:10:38 -0600

    helper files changes

--- text-analysis-service/buildAndRun.bat
+++ text-analysis-service/buildAndRun.bat
@@ -1,9 +1,7 @@
 
-call mvn clean install -f ../text-analysis/pom.xml
-call mvn clean install -f ../text-analysis-twitter/pom.xml
-call mvn clean install -f ../text-analysis-webclient/pom.xml
-call mvn clean install
-call docker build -t com.hkstlr/text-analysis-service .
-SET OPT_PATH=c:/etc/opt/text-analysis-project
-SET WEBCLIENT_PATH=c:/Users/henry.kastler/Documents/GitHub/text-analysis-project/text-analysis-webclient
-call docker rm -f text-analysis-service & docker run -v %OPT_PATH%:/etc/opt/text-analysis-project -v %WEBCLIENT_PATH%:/app/default -p 8080:8080 -p 8443:8443 -p 9990:9990 --name text-analysis-service com.hkstlr/text-analysis-service 
+call mvn clean install -f ../text-analysis/pom.xml %1
+call mvn clean install -f ../text-analysis-twitter/pom.xml %1
+call mvn clean install -f ../text-analysis-webclient/pom.xml %1
+call mvn clean install %1
+call dockerBuildAndRun.bat
+ 
