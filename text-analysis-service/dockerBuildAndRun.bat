f6b75a7e6e23dea070cf2ca46c153840a1b8b731
hkastler <hkastler+github@gmail.com>
2018-11-15 12:40:22 -0600

    webclient path now to webclient-angular

--- text-analysis-service/dockerBuildAndRun.bat
+++ text-analysis-service/dockerBuildAndRun.bat
@@ -1,4 +1,4 @@
 call docker build -t com.hkstlr/text-analysis-service .
 SET OPT_PATH=c:/etc/opt/text-analysis-project
-SET WEBCLIENT_PATH=c:/Users/henry.kastler/Documents/GitHub/text-analysis-project/text-analysis-webclient
+SET WEBCLIENT_PATH=c:/Users/henry.kastler/Documents/GitHub/text-analysis-project/webclient-angular/dist/webclient-angular
 call docker rm -f text-analysis-service & docker run -v %OPT_PATH%:/etc/opt/text-analysis-project -v %WEBCLIENT_PATH%:/app/default -p 8080:8080 -p 8443:8443 -p 9990:9990 --name text-analysis-service com.hkstlr/text-analysis-service
