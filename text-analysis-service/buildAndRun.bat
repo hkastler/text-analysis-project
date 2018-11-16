5b8a3fb3cc5511402e3e7d4352c85bb1ae6dd31c
hkastler <hkastler+github@gmail.com>
2018-11-16 13:24:03 -0600

    deploy to war
    This reverts commit 33ac4710d26b65f63b72ebd013e61e23bbdfe2ad.

--- text-analysis-service/buildAndRun.bat
+++ text-analysis-service/buildAndRun.bat
@@ -1,7 +1,7 @@
 
 call mvn clean install -f ../text-analysis/pom.xml %1
 call mvn clean install -f ../text-analysis-twitter/pom.xml %1
-call mvn clean install -f ../text-analysis-webclient/pom.xml %1
+call mvn clean install -f ../webclient-angular/pom.xml %1
 call mvn clean install %1
 call dockerBuildAndRun.bat
  
--- text-analysis-service/dockerBuildAndRun.bat
+++ text-analysis-service/dockerBuildAndRun.bat
@@ -1,4 +1,5 @@
 call docker build -t com.hkstlr/text-analysis-service .
 SET OPT_PATH=c:/etc/opt/text-analysis-project
-SET WEBCLIENT_PATH=c:/Users/henry.kastler/Documents/GitHub/text-analysis-project/webclient-angular/dist/webclient-angular
+rem SET WEBCLIENT_PATH=c:/Users/henry.kastler/Documents/GitHub/text-analysis-project/webclient-angular/dist/webclient-angular
+SET WEBCLIENT_PATH=c:/etc/opt/text-analysis-project/default-app
 call docker rm -f text-analysis-service & docker run -v %OPT_PATH%:/etc/opt/text-analysis-project -v %WEBCLIENT_PATH%:/app/default -p 8080:8080 -p 8443:8443 -p 9990:9990 --name text-analysis-service com.hkstlr/text-analysis-service
