b202430449098d7be85bbac3596f5c8a52127550
hkastler <hkastler+github@gmail.com>
2018-11-23 07:19:09 -0600

    Loader

---
+++ webclient-react/src/components/loader/Loader.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Loader.css';
+
+class Loader extends React.Component {
+   
+    render() {
+        return (
+            <div id="loader-wrapper">
+                <div id="loader"></div>
+                <div className="loader-section section-left"></div>
+                <div className="loader-section section-right"></div>
+            </div>
+        )
+    }
+}
+
+export default Loader;
