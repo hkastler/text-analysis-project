90dcee9df05c17601d0bb441e7fcba028d77ddf5
hkastler <hkastler+github@gmail.com>
2018-11-23 13:09:39 -0600

    build enhancements

--- text-analysis-service/buildAndRun.bat
+++ text-analysis-service/buildAndRun.bat
@@ -1,7 +1,11 @@
-
+if "%2"=="" (
+    set webclient=webclient-angular
+) else (
+    set webclient=%2
+)
 call mvn clean install -f ../text-analysis/pom.xml %1
 call mvn clean install -f ../text-analysis-twitter/pom.xml %1
-call mvn clean install -f ../webclient-react/pom.xml %1
-call mvn clean install %1
+call mvn clean install -f ../%webclient%/pom.xml %1
+call mvn clean install -f pom.xml -P %webclient%  %1 
 call dockerBuildAndRun.bat
  
--- text-analysis-service/dockerBuildAndRun.bat
+++ text-analysis-service/dockerBuildAndRun.bat
@@ -1,3 +1,3 @@
-call docker build -t com.hkstlr/text-analysis-service .
+call docker build -t com.hkstlr/text-analysis-service . 
 SET OPT_PATH=c:/etc/opt/text-analysis-project
 call docker rm -f text-analysis-service & docker run -v %OPT_PATH%:/etc/opt/text-analysis-project -p 8080:8080 -p 8443:8443 -p 9990:9990 --name text-analysis-service com.hkstlr/text-analysis-service
