701615c350b12b7df0794999715954fdefff2463
hkastler <hkastler+github@gmail.com>
2018-11-23 07:17:36 -0600

    formatting

--- webclient-react/src/components/about/About.js
+++ webclient-react/src/components/about/About.js
@@ -3,19 +3,20 @@ import React from 'react';
 
 
 class About extends React.Component {
-  render() {
-    return (
-    <div class="panel panel-primary">
-    <div class="panel-heading">
-        <h1>About</h1>
-    </div>
-    <p>Machine Learning, Jakarta EE, React, Bootstrap, D3 </p>
-    <p>The github repository for this project is located
-        <a href="https://github.com/hkastler/text-analysis-project" 
-        rel="noopener noreferrer" target="_blank"> here.</a></p>
-  </div>
-  )
-  }
+
+    render() {
+        return (
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h1>About</h1>
+                </div>
+                <p>Machine Learning, Jakarta EE, React, Bootstrap, D3 </p>
+                <p>The github repository for this project is located
+        <a href="https://github.com/hkastler/text-analysis-project"
+                        rel="noopener noreferrer" target="_blank"> here.</a></p>
+            </div>
+        )
+    }
 }
 
 export default About;
