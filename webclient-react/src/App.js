c28d9fdf4cd0753489d7d6bc03d4c3d3145b25c8
hkastler <hkastler+github@gmail.com>
2018-11-23 07:21:26 -0600

    organize imports

--- webclient-react/src/App.js
+++ webclient-react/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom'
+import About from './components/about/About';
 import Navigation from './components/navbar/Navigation';
 import TwitterSentimentAnalysis from './components/twitter-sentiment-analysis/TwitterSentimentAnalysis';
-import About from './components/about/About';
 
 class App extends Component {
+
   render() {
     return (
       <BrowserRouter>
-        <div className="App" class="container">
+        <div className="container" >
           <Navigation />
           <Route exact component={TwitterSentimentAnalysis} path="/" />
           <Route component={TwitterSentimentAnalysis} path="/home" />
