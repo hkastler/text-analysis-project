fe662f7a3f3f7dabbf93c985bb09eeaf802ee97f
hkastler <hkastler+github@gmail.com>
2018-12-04 15:33:14 -0600

    url test

--- webclient-angular/src/environments/environment.spec.ts
+++ webclient-angular/src/environments/environment.spec.ts
@@ -6,12 +6,13 @@ describe('Environment', () => {
 
     it('should create dev', () => {
         expect(dev).toBeTruthy();
-        expect(dev.environment.production).toBe(false); 
+        expect(dev.environment.production).toBe(false);
+        expect(dev.environment.twitterSentimentAnalysisURL).toBe("//localhost:8080/text-analysis-service/rest/twittersa/sa/");
     });
 
     it('should create prod', () => {
         expect(prod).toBeTruthy();
         expect(prod.environment.production).toBe(true);
-        
+        expect(prod.environment.twitterSentimentAnalysisURL).toBe("//localhost:8080/text-analysis-service/rest/twittersa/sa/");
     });
 });
