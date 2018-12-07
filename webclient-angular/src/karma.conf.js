defb025b75db9bb38a7390e77f810bbcf18adbbb
hkastler <hkastler+github@gmail.com>
2018-12-07 17:21:31 -0600

    for jenkins issue

--- webclient-angular/src/karma.conf.js
+++ webclient-angular/src/karma.conf.js
@@ -27,6 +27,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: true
   });
 };
