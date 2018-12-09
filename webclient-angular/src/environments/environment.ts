e75456338241d136cf407f431951383f68470085
hkastler <hkastler+github@gmail.com>
2018-12-09 06:45:30 -0600

    build time properties experiment

--- webclient-angular/src/environments/environment.ts
+++ webclient-angular/src/environments/environment.ts
@@ -1,18 +1,13 @@
+import PROPERTIES from './service-properties.js';
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
-  twitterSentimentAnalysisURL: createTwitterSentimentAnalysisURL()  
+  twitterSentimentAnalysisURL: PROPERTIES.scheme + "://" + PROPERTIES.hostname + ":" + PROPERTIES.port +  PROPERTIES.serviceUrl, 
 };
 
-function createTwitterSentimentAnalysisURL(){
-  var port = window.location.href.indexOf("https://") == 0 ? "8443" : "8080";
-  var serviceLoc = "//" + window.location.hostname + ":" + port;
-  var serviceUrl = '/text-analysis-service/rest/twittersa/sa/';
-  return serviceLoc + serviceUrl;
-}
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
