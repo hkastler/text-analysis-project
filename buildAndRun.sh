4347265d3eb8a747231b9b36759ceb6e60e459f7
hkastler <hkastler+github@gmail.com>
2018-12-11 13:41:51 -0600

    build changes

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-mvn clean install -f ../text-analysis/pom.xml
-mvn clean install -f ../text-analysis-twitter/pom.xml
-mvn clean install -f ../text-analysis-webclient/pom.xml
-mvn clean install && docker build -t com.hkstlr/text-analysis-service .
-OPT_PATH=c:/etc/opt/text-analysis-project
-WEBCLIENT_PATH=c:/Users/henry.kastler/Documents/GitHub/text-analysis-project/text-analysis-webclient
-docker rm -f text-analysis-service || true && docker run -v $OPT_PATH:/etc/opt/text-analysis-project -v $WEBCLIENT_PATH:/app/default -p 8080:8080 -p 8443:8443 --name text-analysis-service com.hkstlr/text-analysis-service 
+mvn clean install -f ./pom.xml -P default
+docker-compose up --build
