fa6b2304f5a94924ea8b6535b0905e331d007e4d
hkastler <hkastler+github@gmail.com>
2018-12-13 12:15:46 -0600

    working hamburger menu for react

--- webclient-react/src/components/navbar/Navigation.js
+++ webclient-react/src/components/navbar/Navigation.js
@@ -3,17 +3,37 @@ import { NavLink } from 'react-router-dom'
 
 
 class Navigation extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      navbarOpen: false
+    };
+    this.render = this.render.bind(this);
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+  }
+
+  toggleNavbar() {
+    this.setState({
+      navbarOpen: !this.state.navbarOpen
+    });
+  }
+
   render() {
+    let collapseMenuClass = ["collapse navbar-collapse"];
+    if (this.state.navbarOpen) {
+      collapseMenuClass.push('show');
+    }
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 
         <NavLink className="navbar-brand" to="/">Twitter Sentiment Analysis</NavLink>
 
-        <button className="navbar-toggler" type="button">
+        <button className="navbar-toggler" type="button" onClick={this.toggleNavbar}>
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="collapse navbar-collapse">
+        <div className={collapseMenuClass.join(' ')} >
           <ul className="navbar-nav mr-auto">
             <li className="nav-item" >
               <NavLink className="nav-link" activeClassName="nav-link active" to="/home">Home</NavLink>
