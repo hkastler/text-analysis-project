160b666dee79b38396e3fefd0ff365bcd22cfce4
hkastler <hkastler+github@gmail.com>
2018-12-26 12:36:19 -0600

    StrictHostKeyChecking no

--- wildfly-resources/ec2/ec2-wildfly-full.sh
+++ wildfly-resources/ec2/ec2-wildfly-full.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #$1=keyfile $2=ipaddr
 #run as sh ec2-wildfly.sh <path-to-pem> <instance-ip>
-ssh -i $1 ec2-user@$2 'bash -s' < ./image-setup.sh && sh ./scp-configs.sh $1 $2 
+ssh -o "StrictHostKeyChecking no" -i $1 ec2-user@$2 'bash -s' < ./image-setup.sh && sh ./scp-configs.sh $1 $2
