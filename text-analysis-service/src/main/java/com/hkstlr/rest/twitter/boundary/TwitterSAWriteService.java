da7088f297c605ab72808f49cd5e463af3a07322
hkastler <hkastler+github@gmail.com>
2019-04-01 09:36:24 -0500

    refactor

--- text-analysis-service/src/main/java/com/hkstlr/rest/twitter/boundary/TwitterSAWriteService.java
+++ text-analysis-service/src/main/java/com/hkstlr/rest/twitter/boundary/TwitterSAWriteService.java
@@ -1,7 +1,6 @@
 package com.hkstlr.rest.twitter.boundary;
 
 import java.io.IOException;
-import java.util.logging.Logger;
 
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.event.Event;
@@ -13,9 +12,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
-import com.hkstlr.rest.control.AbstractEvent;
 import com.hkstlr.rest.control.WriteServiceEvent;
-import com.hkstlr.twitter.control.FileWR;
 
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
@@ -25,9 +22,8 @@ import org.eclipse.microprofile.opentracing.Traced;
 @Path("twttr-sa-write")
 @Traced
 @Timed
+@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class TwitterSAWriteService {
-
-    private static final Logger LOG = Logger.getLogger(TwitterSAWriteService.class.getName());
 
     TweetAnalyzerBean tab;
 
@@ -45,8 +41,7 @@ public class TwitterSAWriteService {
 
     @GET
     @Path("/train/{sentiment}/{tweetText}")
-    @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
-    @Counted(name = "train-text", absolute = true, monotonic = true, description = "Number of times the addTrainText method is requested")
+    @Counted(name = "addTrainText", absolute = true, monotonic = true, description = "Number of times the addTrainText method is requested")
     public String addTrainText(@DefaultValue(value = " ") @PathParam("sentiment") String sentiment,
             @DefaultValue(value = " ") @PathParam("tweetText") String tweetText) throws IOException {
 
